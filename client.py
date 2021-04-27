66416324021c7e8dfc9bb771cb7901bad1aef90f
xTarzx <telmomartins2000@gmail.com>
2021-04-27 19:28:56 +0100

    idfk

--- client.py
+++ client.py
@@ -1,12 +1,27 @@
-import socket
+import socket, _thread
+
+
+class Client(object):
+    def __init__(self, ip, port):
+        self.connect(ip, port)
+    
+    def get_messages(self, s):
+        while True:
+            data = s.recv(1024)
+            if data:
+                print(data)
+
+
+    def connect(self, ip, port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((ip, port))
+            print(f"Connected to {ip}:{port}")
+            _thread.start_new_thread(self.get_messages, (s,))
+            while True:
+                data = input(">> ").encode()
+                s.sendall(data)
 
 HOST = input("Server IP > ")
 PORT = 4040
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while data != 'exit':
-        data = input("message > ")
-        s.sendall(data.encode())
-        received = s.recv(1024)
-        print(received.decode())
+Client(HOST, PORT)
--- server.py
+++ server.py
@@ -1,14 +1,38 @@
-import socket
+import socket, _thread
+
+class Server(object):
+    def __init__(self, ip, port):
+        self.connections = []
+        self.run(ip, port)
+
+    def on_connect(self, c):
+        with c:
+            while True:
+                data = c.recv(1024)
+                if data:
+                    self.on_message(data)
+
+    def on_message(self, data):
+        for connection in self.connections:
+            connection.sendall(data)
+
+    def run(self, ip, port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((ip, port))
+            print("Listening for conections...")
+            s.listen()
+            while True:
+                try:
+                    conn, addr = s.accept()
+                    self.connections.append(conn)
+                    _thread.start_new_thread(self.on_connect, (conn,))
+                except KeyboardInterrupt:
+                    break
+
+
+
 
 HOST = "0.0.0.0"
 PORT = 4040
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print("Connected", addr)
-        data = conn.recv(1024)
-        print(data.decode())
-        conn.sendall(b"Gay")
+Server(HOST, PORT)
